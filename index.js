75f6c7bf3fa6ccc1bc6542429b86c0d6c7c21607
André Bastos <andre.bastos.lei@gmail.com>
2021-03-19 21:12:38 +0000

    final fixes first package to npm

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const Service = require("./src/services/Service");
 module.exports=Nrailsjs;
 module.exports.Controller =Controller;
 module.exports.Service =Service;
+
+//https://bugfender.com/blog/how-to-create-an-npm-package/
